19699810ed09a7b36347604d1e58fa9e4b7b1405
naumshubaev <naum.shubaev@gmail.com>
2021-10-01 16:41:08 +0300

    v1.5

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceKopeiks = 160750_00;
+        int kopeiksPerMile = 20_00;
+
+        int milesCount = priceKopeiks / kopeiksPerMile;
+
+        System.out.println(milesCount);
+    }
+}
